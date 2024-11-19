d5e6950709514c32197adf44df33a4aa273d2154
Lei Song <lei.song@microsoft.com>
2024-11-19 01:23:31 +0000

    u

--- run_exp.sh
+++ run_exp.sh
@@ -1,12 +1,13 @@
 
 
-for algo in SFTReg simpo alphaDPO
-do
-    conda run -n dataenv python scripts/init_decode_data.py --train_model meta-llama/Llama-3.2-3B-Instruct --ref_model meta-llama/Llama-3.2-3B-Instruct --epoch 1 --algo $algo --num_samples 40000 --debug;
+conda run -n dataenv python scripts/init_decode_data.py --train_model meta-llama/Llama-3.2-3B-Instruct --ref_model meta-llama/Llama-3.2-3B-Instruct --epoch 1 --algo cpo --num_samples 40000 --debug;
 
-    for i in {1..6}; do \
+for i in {1..6}
+do
+    for algo in SFTReg simpo alphaDPO
+    do
+        conda run -n dataenv python scripts/decode_data.py --ref_model meta-llama/Llama-3.2-3B-Instruct --train_model /home/lesong/codes/SimPO/outputs/$algo_$i --epoch $i  --algo $algo;\
         conda run -n simpo python scripts/run_simpo.py training_configs/llama-3-3b-instruct-$algo-v2.yaml epoch=$i;\
         conda run -n simpo python scripts/run_simpo_eval.py training_configs/llama-3-3b-instruct-$algo-v2.yaml  epoch=$i exp_name=$algo-$i;\
-        conda run -n dataenv python scripts/decode_data.py --ref_model meta-llama/Llama-3.2-3B-Instruct --train_model /home/lesong/codes/SimPO/outputs/$algo_$i --epoch $((i+1))  --algo $algo;\
     done
 done
